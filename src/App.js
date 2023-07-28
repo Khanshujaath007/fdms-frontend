fe206eac5dc0230b299cb2c021da959b162b8eb3
khanshujaath007 <khanshujaath007@gmail.com>
2023-07-28 23:59:36 +0530

    Signup Form

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
 import "./App.css";
-import { useState } from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
-import Multistepbar from "./components/Multistepbar.js";
-import Multistepform from "./components/Multistepform.js";
-import questions from "./Questions.js"
-import Loginform from "./components/LoginForm/Loginform.js";
+import Signup from "./components/SignupForm/Signup";
 function App() {
-  const [index, setIndex] = useState(1);
-  const totalpagecount = questions.length;
-  const prevButton = () => {
-    if (index > 1) setIndex((prevIndex) => prevIndex - 1);
-  };
-  const nextButton = () => {
-    if (index < 3) setIndex((prevIndex) => prevIndex + 1);
-  };
   return (
     <div className="App">
-      <Container className="h-100">
-        <Row className="m-5">
-          <Col className="align-self-center">
-            <Multistepbar step={index} />
-          </Col>
-        </Row>
-        <Row>
-          <Card>
-            <Card.Body>
-              <Multistepform step={index} list={questions} />
-            </Card.Body>
-            <Card.Footer className="d-flex justify-content-between ">
-              <Button onClick={prevButton} disabled={index === 1}>
-                Previous
-              </Button>
-              <Button onClick={nextButton}>
-                {totalpagecount === index ? "Submit" : "Next"}
-              </Button>
-            </Card.Footer>
-          </Card>
-        </Row>
-      </Container>
+      <Signup></Signup>
     </div>
   );
 }
--- src/components/SignupForm/Multistepbar.js
+++ src/components/SignupForm/Multistepbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "react-step-progress-bar/styles.css";
+
 import { ProgressBar, Step } from "react-step-progress-bar";
 import "./Multistepbar.css";
 

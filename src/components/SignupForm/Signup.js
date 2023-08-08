bac5c765b8a6ec7e101904799bd70d2908c6737a
khanshujaath007 <khanshujaath007@gmail.com>
2023-08-08 20:37:26 +0530

    Recreated Signup form

--- src/components/SignupForm/Signup.js
+++ src/components/SignupForm/Signup.js
@@ -1,82 +1,114 @@
-import { useState } from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./Signup.css";
-import Multistepbar from "./Multistepbar.js";
-import Multistepform from "./Multistepform.js";
-import questions from "../../Questions.js";
-function Signup() {
-  const [index, setIndex] = useState(1);
-  const [pagesAnswers, setPagesAnswers] = useState({});
-  const totalpagecount = questions.length;
 
-  const navigate = useNavigate();
-
-  const onPagesAnswerUpdate = (step, answerObj) => {
-    setPagesAnswers({ ...pagesAnswers, [step]: answerObj });
+const Signup = () => {
+  const initialValues = { username: "", email: "", password: "", phone: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
   };
 
-  const prevButton = () => {
-    if (index > 1) setIndex((prevIndex) => prevIndex - 1);
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
   };
-
-  const nextButton = async () => {
-    if (index < totalpagecount) {
-      setIndex((prevIndex) => prevIndex + 1);
-    } else {
-      try {
-        const response = await fetch('http://localhost:5500/register', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(pagesAnswers),
-        });
-
-        if (response.ok) {
-          console.log('Registration successful!');
-          navigate('/faculty-dashboard');
-        } else {
-          console.log('Registration failed!');
-          // console.log(pagesAnswers[1].firstName)
-        }
-      } catch (error) {
-        console.error('Error registering user:', error);
-      }
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit === true) {
+      console.log(formValues);
     }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const emailregex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/;
+    const nonNumericPattern = /[^\d]/;
+    if (!values.username) {
+      errors.username = "Username is required";
+    } else if (values.username.length < 3) {
+      errors.username = "Username too short!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!emailregex.test(values.email)) {
+      errors.email = "Enter a valid Email-ID ";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 4) {
+      errors.password = "Password too short!";
+    }
+    if (!values.phone) {
+      errors.phone = "Phone Number is required";
+    } else if (nonNumericPattern.test(values.phone)) {
+      errors.phone = "Enter a valid Phone number";
+    }
+    return errors;
   };
-
   return (
     <>
-      <Container className="h-100">
-        <Row className="m-5">
-          <Col className="align-self-center">
-            <Multistepbar step={index} />
-          </Col>
-        </Row>
-        <Row className="d-flex justify-content-center">
-          <Card className="m-0">
-            <Card.Body>
-              <Multistepform
-                step={index}
-                list={questions}
-                onPageUpdate={onPagesAnswerUpdate}
-                pagesAnswers={pagesAnswers}
-              />
-            </Card.Body>
-            <Card.Footer className="d-flex justify-content-between ">
-              <Button onClick={prevButton} disabled={index === 1} className="prev">
-                Previous
-              </Button>
-              <Button onClick={nextButton} className="next">
-                {totalpagecount === index ? "Submit" : "Next"}
-              </Button>
-            </Card.Footer>
-          </Card>
-        </Row>
-      </Container>
+      {Object.keys(formErrors).length === 0 && isSubmit === true ? (
+        <div className="ui message success">Signed in Successfully</div>
+      ) : (
+        <div className="container">
+          <form onSubmit={onSubmitHandler}>
+            <h1>Sign up</h1>
+            <div className="ui-divider"></div>
+            <div className="ui form">
+              <div className="field">
+                <label>Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Enter your name"
+                  value={formValues.username}
+                  onChange={onChangeHandler}
+                ></input>
+                <span className="error-span">{formErrors.username}</span>
+              </div>
+              <div className="field">
+                <label>Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter your Email ID"
+                  value={formValues.email}
+                  onChange={onChangeHandler}
+                ></input>
+                <span className="error-span">{formErrors.email}</span>
+              </div>
+              <div className="field">
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Enter a password"
+                  value={formValues.password}
+                  onChange={onChangeHandler}
+                ></input>
+                <span className="error-span">{formErrors.password}</span>
+              </div>
+              <div className="field">
+                <label>Phone Number</label>
+                <input
+                  type="tel"
+                  name="phone"
+                  placeholder="Enter Mobile no."
+                  value={formValues.phone}
+                  onChange={onChangeHandler}
+                ></input>
+                <span className="error-span">{formErrors.phone}</span>
+              </div>
+              <button className="fluid ui button">Submit</button>
+            </div>
+          </form>
+        </div>
+      )}
     </>
   );
-}
+};
 
 export default Signup;

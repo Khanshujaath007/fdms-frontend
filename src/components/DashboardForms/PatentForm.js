34d63e43a5ae27591cbed6f0178d705e15a322a8
khanshujaath007 <khanshujaath007@gmail.com>
2023-08-12 15:24:36 +0530

    publication and patent form validation

--- src/components/DashboardForms/PatentForm.js
+++ src/components/DashboardForms/PatentForm.js
@@ -1,7 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import useInput from "../hooks/use-input";
 import "./Form.css";
 const ModalOverlay = (props) => {
+  let formIsValid = true;
+  //patent name
+  const {
+    value: enteredPatent,
+    isValid: enteredPatentIsValid,
+    hasError: patentInputHasError,
+    valueChangeHandler: patentChangedHandler,
+    inputBlurHandler: patentBlurHandler,
+    reset: resetPatentInput,
+  } = useInput((value) => value.trim() !== "");
+  //patent link
+  const {
+    value: enteredPatentLink,
+    isValid: enteredPatentlinkIsValid,
+    hasError: patentlinkInputHasError,
+    valueChangeHandler: patentlinkChangedHandler,
+    inputBlurHandler: patentlinkBlurHandler,
+    reset: resetPatentlinkInput,
+  } = useInput((value) => value.includes("https://"));
+  //patent date
+  const {
+    value: enteredPatentdate,
+    isValid: enteredPatentdateIsValid,
+    hasError: patentdateInputHasError,
+    valueChangeHandler: patentdateChangedHandler,
+    inputBlurHandler: patentdateBlurHandler,
+    reset: resetPatentdateInput,
+  } = useInput((value) => value.trim() !== "");
+
+  const submissionHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredPatent, enteredPatentLink, enteredPatentdate);
+    resetPatentInput("");
+    resetPatentdateInput("");
+  };
+  if (
+    enteredPatentIsValid &&
+    enteredPatentdateIsValid &&
+    enteredPatentlinkIsValid
+  )
+    formIsValid = true;
   return (
     <>
       <div className="modalBackground">
@@ -13,26 +55,50 @@ const ModalOverlay = (props) => {
             <h1>Fill Patent Details</h1>
           </div>
           <div className="body">
-            <form>
+            <form onSubmit={submissionHandler}>
               <div>
                 <label>Enter Patent Name</label>
-                <input type="text"></input>
+                <input
+                  type="text"
+                  value={enteredPatent}
+                  onChange={patentChangedHandler}
+                  onBlur={patentBlurHandler}
+                ></input>
+                {patentInputHasError && (
+                  <p className="error-text">patent name cannot be empty</p>
+                )}
               </div>
               <div>
                 <label>Enter Patent date</label>
-                <input type="date"></input>
+                <input
+                  type="date"
+                  value={enteredPatentdate}
+                  onChange={patentdateChangedHandler}
+                  onBlur={patentdateBlurHandler}
+                ></input>
+                {patentdateInputHasError && (
+                  <p className="error-text">patent date cannot be empty</p>
+                )}
               </div>
               <div>
                 <label>Enter Patent Link</label>
-                <input type="text"></input>
+                <input
+                  type="text"
+                  value={enteredPatentLink}
+                  onChange={patentlinkChangedHandler}
+                  onBlur={patentlinkBlurHandler}
+                ></input>
+              </div>
+              {patentlinkInputHasError && (
+                <p className="error-text">link should be valid</p>
+              )}
+              <div className="footer">
+                <button onClick={() => props.closeModal(false)} id="cancelBtn">
+                  Cancel
+                </button>
+                <button disabled={!formIsValid}>Submit</button>
               </div>
             </form>
-          </div>
-          <div className="footer">
-            <button onClick={() => props.closeModal(false)} id="cancelBtn">
-              Cancel
-            </button>
-            <button>Submit</button>
           </div>
         </div>
       </div>
--- src/components/DashboardForms/PublicationForm.js
+++ src/components/DashboardForms/PublicationForm.js
@@ -1,7 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./Form.css";
+import useInput from "../hooks/use-input";
 const ModalOverlay = (props) => {
+  let formIsValid = false;
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameChangedHandler,
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredDate,
+    isValid: enteredDateIsValid,
+    hasError: dateInputHasError,
+    valueChangeHandler: dateChangeHandler,
+    inputBlurHandler: dateBlurHandler,
+    reset: resetDateInput,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredLink,
+    isValid: enteredLinkIsValid,
+    hasError: linkInputHasError,
+    valueChangeHandler: linkChangeHandler,
+    inputBlurHandler: linkBlurHandler,
+    reset: resetLinkInput,
+  } = useInput((value) => value.includes("https://"));
+
+  if (enteredNameIsValid && enteredDateIsValid && enteredLinkIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    if (!formIsValid) return;
+    console.log(enteredName);
+    console.log(enteredDate);
+    console.log(enteredLink);
+    resetNameInput("");
+    resetDateInput("");
+    resetLinkInput("");
+  };
   return (
     <>
       <div className="modalBackground">
@@ -13,26 +56,58 @@ const ModalOverlay = (props) => {
             <h1>Fill Publication Details</h1>
           </div>
           <div className="body">
-            <form>
-              <div>
+            <div>
+              <form onSubmit={formSubmissionHandler}>
                 <label>Enter Publication Name</label>
-                <input type="text"></input>
-              </div>
-              <div>
-                <label>Enter Publication date</label>
-                <input type="date"></input>
-              </div>
-              <div>
-                <label>Enter Publication Link</label>
-                <input type="text"></input>
-              </div>
-            </form>
-          </div>
-          <div className="footer">
-            <button onClick={() => props.closeModal(false)} id="cancelBtn">
-              Cancel
-            </button>
-            <button>Submit</button>
+                <input
+                  type="text"
+                  id="name"
+                  onChange={nameChangedHandler}
+                  onBlur={nameBlurHandler}
+                  value={enteredName}
+                ></input>
+                {nameInputHasError && (
+                  <p className="error-text">Name cannot be empty</p>
+                )}
+
+                <div>
+                  <label>Enter Publication date</label>
+                  <input
+                    type="date"
+                    id="date"
+                    onChange={dateChangeHandler}
+                    onBlur={dateBlurHandler}
+                    value={enteredDate}
+                  ></input>
+                  {dateInputHasError && (
+                    <p className="error-text">Date cannot be empty</p>
+                  )}
+                </div>
+                <div>
+                  <label>Enter Publication Link</label>
+                  <input
+                    type="text"
+                    onChange={linkChangeHandler}
+                    onBlur={linkBlurHandler}
+                    value={enteredLink}
+                  ></input>
+                  {linkInputHasError && (
+                    <p className="error-text">
+                      Link should start with https://
+                    </p>
+                  )}
+                </div>
+                <div className="footer">
+                  <button
+                    onClick={() => props.closeModal(false)}
+                    id="cancelBtn"
+                  >
+                    Cancel
+                  </button>
+                  <button disabled={!formIsValid}>Submit</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
